0735c1f478392104185809a50d1bd140dc94f32f
Nguyễn Minh Khoa <khoa.nguyenbkhcm@hcmut.edu.vn>
2025-04-10 17:40:41 +0700

    react app created with create-react-app

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Navbar.css'; // Optional: Add styles for the navbar
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <a href="/">LibraryApp</a>
+      </div>
+      <ul className="navbar-links">
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/books">Books</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
